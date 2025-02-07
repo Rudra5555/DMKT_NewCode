6dac7a6c82f69395b36fdae9f1da93c884c5d6d0
Biplob-m <dasbiplob8116.com>
2025-02-07 20:50:52 +0530

    loginApi

--- src/app/feature-module/auth/login/login.component.ts
+++ src/app/feature-module/auth/login/login.component.ts
@@ -44,7 +44,7 @@ export class LoginComponent implements OnInit {
   errorMsg:any;
   toaster: any;
   timestamp: string ='';
-
+  modalMessage: string = '';
   constructor(private router: Router,private loginService : LoginComponentService,private dataService : DataService ,private formBuilder: FormBuilder) {}
 
 
@@ -87,164 +87,164 @@ get formFields(){return this.loginForm.controls; }
 
 
 
-  onClickSubmit(formData: any){
-// alert("called 1st")
-if (this.loginForm.invalid) {
-   this.loginForm.markAllAsTouched();
-    return;
+//   onClickSubmit(formData: any){
+// // alert("called 1st")
+// if (this.loginForm.invalid) {
+//    this.loginForm.markAllAsTouched();
+//     return;
   
-}
+// }
 
- let loginData={
-  'userName': this.loginForm.controls['emailId'].value,
-  'password': this.loginForm.controls['password'].value
- }
+//  let loginData={
+//   'userName': this.loginForm.controls['emailId'].value,
+//   'password': this.loginForm.controls['password'].value
+//  }
 
 
 
-if(loginData){
- // //console.log("hii")
-  //console.log("payload ::" ,JSON.stringify(loginData))
-  this.loginService.login(loginData).subscribe({
-    next: (event: any) => {
-      if (event instanceof HttpResponse) {
-        this.data = event.body
-        // console.log("jjjjjjjjjj",JSON.stringify(this.data))
-      //  //console.log(resp.value.role)
+// if(loginData){
+//  // //console.log("hii")
+//   //console.log("payload ::" ,JSON.stringify(loginData))
+//   this.loginService.login(loginData).subscribe({
+//     next: (event: any) => {
+//       if (event instanceof HttpResponse) {
+//         this.data = event.body
+//         // console.log("jjjjjjjjjj",JSON.stringify(this.data))
+//       //  //console.log(resp.value.role)
        
-        if(this.data!=null){
+//         if(this.data!=null){
          
-          if(this.data.response!=null){
-            this.getRoleData = this.data.response.role;
+//           if(this.data.response!=null){
+//             this.getRoleData = this.data.response.role;
 
-            // console.log("kkkkkkkkkkk",this.getRoleData)
+//             // console.log("kkkkkkkkkkk",this.getRoleData)
 
-            localStorage.setItem('role', this.getRoleData)
-          }
-          else{
-            // this.bottomCenter()
-            this.errorFlg=true;
-            this.errorMsg="Invalid UserName Or Password!!!!!"
+//             localStorage.setItem('role', this.getRoleData)
+//           }
+//           else{
+//             // this.bottomCenter()
+//             this.errorFlg=true;
+//             this.errorMsg="Invalid UserName Or Password!!!!!"
 
-            setTimeout(()=>{
-              this.resetErrFlag()
-              }, 2000);
+//             setTimeout(()=>{
+//               this.resetErrFlag()
+//               }, 2000);
             
-          }
+//           }
        
           
-          // //console.log("inside datattaaaaaaaaa")
-        //  //console.log(resp.value.role)
-         // sessionStorage.setItem('user_role', resp.value.role);
-          // const observable = from(this.data.response.role);
+//           // //console.log("inside datattaaaaaaaaa")
+//         //  //console.log(resp.value.role)
+//          // sessionStorage.setItem('user_role', resp.value.role);
+//           // const observable = from(this.data.response.role);
  
-          // this.dataService.send_data.next(this.data.response.role)
+//           // this.dataService.send_data.next(this.data.response.role)
 
-         // localStorage.setItem('user_role', resp.value.role);
+//          // localStorage.setItem('user_role', resp.value.role);
 
-         if(this.getRoleData == "Admin"){
+//          if(this.getRoleData == "Admin"){
 
-          // this.router.navigate([routes.adminDashboard],{queryParams : this.data.value});
-          // localStorage.setItem("loggedUserName",this.data.response.userName);
+//           // this.router.navigate([routes.adminDashboard],{queryParams : this.data.value});
+//           // localStorage.setItem("loggedUserName",this.data.response.userName);
 
-          const userId = this.data.response.userId;
-          // //console.log("userID",userId)
-          const userName = this.data.response.userName; 
-          //this.router.navigate([routes.adminDashboard], { queryParams: { userId: userId } });
+//           const userId = this.data.response.userId;
+//           // //console.log("userID",userId)
+//           const userName = this.data.response.userName; 
+//           //this.router.navigate([routes.adminDashboard], { queryParams: { userId: userId } });
 
-          const secretKey = this.timestamp;
-          const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response.userId), secretKey).toString();
-          console.log("encrypted::",encryptedParam);
-          this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
+//           const secretKey = this.timestamp;
+//           const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response.userId), secretKey).toString();
+//           console.log("encrypted::",encryptedParam);
+//           this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
           
         
-          localStorage.setItem("loggedUserName", userName);
+//           localStorage.setItem("loggedUserName", userName);
           
-          // const userId = this.data.response.userId;
-          localStorage.setItem("loggedInUserId",userId);
+//           // const userId = this.data.response.userId;
+//           localStorage.setItem("loggedInUserId",userId);
 
-        }
+//         }
 
 
-        if(this.getRoleData == "Librarian"){
+//         if(this.getRoleData == "Librarian"){
 
-          const userId = this.data.response.userId;
+//           const userId = this.data.response.userId;
 
-          localStorage.setItem("loggedInUserId",userId);
+//           localStorage.setItem("loggedInUserId",userId);
         
-          //  this.router.navigate([routes.employee],{queryParams : this.data.value})
-          const secretKey = this.timestamp;
-          const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.value), secretKey).toString();
-          console.log("encrypted::",encryptedParam);
-          this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
+//           //  this.router.navigate([routes.employee],{queryParams : this.data.value})
+//           const secretKey = this.timestamp;
+//           const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.value), secretKey).toString();
+//           console.log("encrypted::",encryptedParam);
+//           this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
 
-          localStorage.setItem("loggedUserName",this.data.response.userName);
+//           localStorage.setItem("loggedUserName",this.data.response.userName);
 
           
-        }
+//         }
 
-        if(this.getRoleData == "User"){
-          const userId = this.data.response.userId;
-          // localStorage.setItem("loggedUserId",userId);
+//         if(this.getRoleData == "User"){
+//           const userId = this.data.response.userId;
+//           // localStorage.setItem("loggedUserId",userId);
 
-          localStorage.setItem("loggedInUserId",userId)
+//           localStorage.setItem("loggedInUserId",userId)
           
 
-          const navigationExtras:NavigationExtras = {
-            state:{
-              username:this.getRoleData
-            }
-          }
-         localStorage.setItem("loggedUserName",this.data.response.userName);
-         const secretKey = this.timestamp;
-         const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response), secretKey).toString();
-         console.log("encrypted::",encryptedParam);
+//           const navigationExtras:NavigationExtras = {
+//             state:{
+//               username:this.getRoleData
+//             }
+//           }
+//          localStorage.setItem("loggedUserName",this.data.response.userName);
+//          const secretKey = this.timestamp;
+//          const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response), secretKey).toString();
+//          console.log("encrypted::",encryptedParam);
          
 
-          this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
+//           this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
 
-          // this.router.navigate([routes.employee],{queryParams : this.data.response})
-        }
-        if(this.getRoleData == "SuperUser"){
-          this.router.navigate([routes.employee],{queryParams : this.data.response})
-          console.log("super user",this.getRoleData)
-          const secretKey = this.timestamp;
-          const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response), secretKey).toString();
-          console.log("encrypted::",encryptedParam);
+//           // this.router.navigate([routes.employee],{queryParams : this.data.response})
+//         }
+//         if(this.getRoleData == "SuperUser"){
+//           this.router.navigate([routes.employee],{queryParams : this.data.response})
+//           console.log("super user",this.getRoleData)
+//           const secretKey = this.timestamp;
+//           const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response), secretKey).toString();
+//           console.log("encrypted::",encryptedParam);
           
         
-          localStorage.setItem("loggedUserName",this.data.response.userName);
-          const userId = this.data.response.userId;
-          localStorage.setItem("loggedSuperUserId",userId);
-          localStorage.setItem("loggedInUserId",userId);
-        }
+//           localStorage.setItem("loggedUserName",this.data.response.userName);
+//           const userId = this.data.response.userId;
+//           localStorage.setItem("loggedSuperUserId",userId);
+//           localStorage.setItem("loggedInUserId",userId);
+//         }
 
-        if(this.getRoleData == "HOD"){
+//         if(this.getRoleData == "HOD"){
 
-          const hodId = this.data.response.userId;
-          localStorage.setItem("loggedHodId",hodId);
-          localStorage.setItem("loggedInUserId",hodId)
-          //console.log("hodID",hodId)
-          const navigationExtras:NavigationExtras = {
-            state:{
-              username:this.getRoleData
-            }
-          }
-         localStorage.setItem("loggedUserName",this.data.response.userName);
-         const secretKey =  this.timestamp;
-         const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response), secretKey).toString();
-         console.log("encrypted::",encryptedParam);
+//           const hodId = this.data.response.userId;
+//           localStorage.setItem("loggedHodId",hodId);
+//           localStorage.setItem("loggedInUserId",hodId)
+//           //console.log("hodID",hodId)
+//           const navigationExtras:NavigationExtras = {
+//             state:{
+//               username:this.getRoleData
+//             }
+//           }
+//          localStorage.setItem("loggedUserName",this.data.response.userName);
+//          const secretKey =  this.timestamp;
+//          const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response), secretKey).toString();
+//          console.log("encrypted::",encryptedParam);
          
 
-          this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
-        }
+//           this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
+//         }
 
        
 
-        }
+//         }
       
        
-       // alert(this.getRoleData)
+//        // alert(this.getRoleData)
       
 
 
@@ -254,60 +254,200 @@ if(loginData){
 
 
        
-        const msg = JSON.stringify(event) + ": Successful!";
-        // this.message.push(msg);
-        //console.log(msg)
-      //  this.fileInfos = this.uploadService.getFiles();
-      }
-    },
-    error: (err: any) => {
-      let msg = loginData + ": Failed!";
+//         const msg = JSON.stringify(event) + ": Successful!";
+//         // this.message.push(msg);
+//         //console.log(msg)
+//       //  this.fileInfos = this.uploadService.getFiles();
+//       }
+//     },
+//     error: (err: any) => {
+//       let msg = loginData + ": Failed!";
 
-      if (err.error && err.error.message) {
-        msg += " " + err.error.message;
-      }
+//       if (err.error && err.error.message) {
+//         msg += " " + err.error.message;
+//       }
 
-      this.message.push(msg);
-     // this.fileInfos = this.uploadService.getFiles();
-    }
-  });
+//       this.message.push(msg);
+//      // this.fileInfos = this.uploadService.getFiles();
+//     }
+//   });
 
   
 
 
-} //end if
+// } //end if
 
 
-/*if(this.getRoleData == "Admin"){
-  this.router.navigate([routes.adminDashboard]);
+// /*if(this.getRoleData == "Admin"){
+//   this.router.navigate([routes.adminDashboard]);
 
-}
-if(this.getRoleData == "Librarian"){
+// }
+// if(this.getRoleData == "Librarian"){
 
-  this.router.navigate([routes.leadDashboard])
-}
-if(this.getRoleData == "User"){
-  this.router.navigate([routes.employee])
-}
-if(this.getRoleData == "SuperUser"){
-  this.router.navigate([routes.employee])
-}*/
+//   this.router.navigate([routes.leadDashboard])
+// }
+// if(this.getRoleData == "User"){
+//   this.router.navigate([routes.employee])
+// }
+// if(this.getRoleData == "SuperUser"){
+//   this.router.navigate([routes.employee])
+// }*/
  
  
-//  const t = (localStorage.getItem('EmailId'));
-//     //console.log(t)
+// //  const t = (localStorage.getItem('EmailId'));
+// //     //console.log(t)
   
-    // //console.log(this.loginForm.controls['emailId'].value)
-    // const test = this.loginForm.controls['emailId'].value
+//     // //console.log(this.loginForm.controls['emailId'].value)
+//     // const test = this.loginForm.controls['emailId'].value
 
-    // //console.log("onClick",test)
-    // sessionStorage.setItem('Email', test);
-    // localStorage.setItem('EmailId', test );
-    // //console.log("localShtorage value: ",localStorage.getItem('EmailId' ));
-    // //console.log("SessionShtorage value: ",sessionStorage.getItem('Email'))
-    this.test();
+//     // //console.log("onClick",test)
+//     // sessionStorage.setItem('Email', test);
+//     // localStorage.setItem('EmailId', test );
+//     // //console.log("localShtorage value: ",localStorage.getItem('EmailId' ));
+//     // //console.log("SessionShtorage value: ",sessionStorage.getItem('Email'))
+//     this.test();
+//   }
+  
+onClickSubmit(formData: any){
+  // alert("called 1st")
+  if (this.loginForm.invalid) {
+     this.loginForm.markAllAsTouched();
+      return;
+    
   }
   
+   let loginData={
+    'userName': this.loginForm.controls['emailId'].value,
+    'password': this.loginForm.controls['password'].value
+   }
+  
+  
+  
+  if(loginData){
+    this.loginService.login(loginData).subscribe({
+      next: (event: any) => {
+        if (event instanceof HttpResponse) {
+           this.data = event.body
+  
+         
+          if(this.data!=null){
+           
+            if(this.data.status ===417){
+
+              this.errorFlg=true;
+              this.errorMsg="Invalid UserName Or Password!!!!!"
+  
+              setTimeout(()=>{
+                this.resetErrFlag()
+                }, 2000);
+
+            }
+            else if(this.data.status === 200 && this.data.response === null){
+              this.modalMessage =this.data.message;
+              alert(this.modalMessage)
+            }
+            else{
+              this.getRoleData = this.data.response.role;
+              localStorage.setItem('role', this.getRoleData)
+            }
+         
+            
+  
+           if(this.getRoleData == "Admin"){
+
+            const userId = this.data.response.userId;
+            const userName = this.data.response.userName; 
+            const secretKey = this.timestamp;
+            const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response.userId), secretKey).toString();
+            console.log("encrypted::",encryptedParam);
+            this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }}) 
+            localStorage.setItem("loggedUserName", userName);
+            localStorage.setItem("loggedInUserId",userId);
+  
+          }
+  
+  
+          if(this.getRoleData == "Librarian"){
+  
+            const userId = this.data.response.userId;  
+            localStorage.setItem("loggedInUserId",userId);
+            const secretKey = this.timestamp;
+            const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.value), secretKey).toString();
+            console.log("encrypted::",encryptedParam);
+            this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})   
+            localStorage.setItem("loggedUserName",this.data.response.userName);        
+          }
+  
+          if(this.getRoleData == "User"){
+            const userId = this.data.response.userId;
+            localStorage.setItem("loggedInUserId",userId)
+            const navigationExtras:NavigationExtras = {
+              state:{
+                username:this.getRoleData
+              }
+            }
+           localStorage.setItem("loggedUserName",this.data.response.userName);
+           const secretKey = this.timestamp;
+           const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response), secretKey).toString();
+           console.log("encrypted::",encryptedParam);     
+            this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
+          }
+
+          if(this.getRoleData == "SuperUser"){
+            this.router.navigate([routes.employee],{queryParams : this.data.response})
+            console.log("super user",this.getRoleData)
+            const secretKey = this.timestamp;
+            const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response), secretKey).toString();
+            console.log("encrypted::",encryptedParam);
+
+            localStorage.setItem("loggedUserName",this.data.response.userName);
+            const userId = this.data.response.userId;
+            localStorage.setItem("loggedSuperUserId",userId);
+            localStorage.setItem("loggedInUserId",userId);
+          }
+  
+          if(this.getRoleData == "HOD"){
+  
+            const hodId = this.data.response.userId;
+            localStorage.setItem("loggedHodId",hodId);
+            localStorage.setItem("loggedInUserId",hodId)
+            const navigationExtras:NavigationExtras = {
+              state:{
+                username:this.getRoleData
+              }
+            }
+           localStorage.setItem("loggedUserName",this.data.response.userName);
+           const secretKey =  this.timestamp;
+           const encryptedParam = CryptoJS.AES.encrypt(JSON.stringify(this.data.response), secretKey).toString();
+           console.log("encrypted::",encryptedParam);
+           
+  
+            this.router.navigate([routes.employee],{queryParams : { param: encryptedParam }})
+          }
+      
+  
+          }
+
+         
+          const msg = JSON.stringify(event) + ": Successful!";
+        }
+      },
+      error: (err: any) => {
+        let msg = loginData + ": Failed!";
+  
+        if (err.error && err.error.message) {
+          msg += " " + err.error.message;
+        }
+  
+        this.message.push(msg);
+     
+      }
+    });
+  
+  
+  }
+
+}
 
   navigate() {
     this.router.navigate([routes.adminDashboard]);
