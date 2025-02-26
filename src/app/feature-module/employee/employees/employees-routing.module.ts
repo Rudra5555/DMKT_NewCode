f3ba9231a3156ff016f0a7c647c5b3bd0fdec598
Rudra <nigamrudra761@gmail.com>
2025-02-26 19:23:36 +0530

    bug fix and code clean

--- src/app/feature-module/employee/employees/employees-routing.module.ts
+++ src/app/feature-module/employee/employees/employees-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmployeeListComponent } from './all-employee/employee-list/employee-list.component';
-import { EmployeePageContentComponent } from './all-employee/employee-page-content/employee-page-content.component';
+// import { EmployeeListComponent } from './all-employee/employee-list/employee-list.component';
+// import { EmployeePageContentComponent } from './all-employee/employee-page-content/employee-page-content.component';
 import { EmployeeProfileComponent } from './all-employee/employee-profile/employee-profile.component';
-import { AttendanceAdminComponent } from './attendance-admin/attendance-admin.component';
-import { AttendanceEmployeeComponent } from './attendance-employee/attendance-employee.component';
-import { DepartmentsComponent } from './departments/departments.component';
-import { DesignationsComponent } from './designations/designations.component';
+// import { AttendanceAdminComponent } from './attendance-admin/attendance-admin.component';
+// import { AttendanceEmployeeComponent } from './attendance-employee/attendance-employee.component';
+// import { DepartmentsComponent } from './departments/departments.component';
+// import { DesignationsComponent } from './designations/designations.component';
 import { EmployeesComponent } from './employees.component';
-import { HolidaysComponent } from './holidays/holidays.component';
-import { LeaveAdminComponent } from './leave-admin/leave-admin.component';
-import { LeaveEmployeeComponent } from './leave-employee/leave-employee.component';
-import { LeaveSettingsComponent } from './leave-settings/leave-settings.component';
-import { OvertimeComponent } from './overtime/overtime.component';
-import { ShiftListComponent } from './shift-list/shift-list.component';
-import { ShiftScheduleComponent } from './shift-schedule/shift-schedule.component';
-import { TimesheetComponent } from './timesheet/timesheet.component';
+// import { HolidaysComponent } from './holidays/holidays.component';
+// import { LeaveAdminComponent } from './leave-admin/leave-admin.component';
+// import { LeaveEmployeeComponent } from './leave-employee/leave-employee.component';
+// import { LeaveSettingsComponent } from './leave-settings/leave-settings.component';
+// import { OvertimeComponent } from './overtime/overtime.component';
+// import { ShiftListComponent } from './shift-list/shift-list.component';
+// import { ShiftScheduleComponent } from './shift-schedule/shift-schedule.component';
+// import { TimesheetComponent } from './timesheet/timesheet.component';
 import { AuthGuard } from '../../main/dashboard/AuthGuard';
 
 const routes: Routes = [
