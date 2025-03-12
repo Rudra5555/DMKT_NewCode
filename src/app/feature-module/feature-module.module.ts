b918c16c36c0875f1aed46de47fac3b97f49cf5f
Rudra <nigamrudra761@gmail.com>
2025-03-12 20:16:41 +0530

    code clean 1.7

--- src/app/feature-module/feature-module.module.ts
+++ src/app/feature-module/feature-module.module.ts
@@ -4,12 +4,12 @@ import { SharedModule } from '../shared/sharedIndex';
 import { FeatureModuleRoutingModule } from './feature-module-routing.module';
 import { FeatureModuleComponent } from './feature-module.component';
 import { SideMenuOneComponent } from './common/side_menus/side-menu-one/side-menu-one.component';
-import { SideMenuTwoComponent } from './common/side_menus/side-menu-two/side-menu-two.component';
+// import { SideMenuTwoComponent } from './common/side_menus/side-menu-two/side-menu-two.component';
 import { HeaderOneComponent } from './common/headers/header-one/header-one.component';
 // import { SideMenuThreeComponent } from './common/side_menus/side-menu-three/side-menu-three.component';
 import { SettingsMenuComponent } from './common/settings-menu/settings-menu.component';
-import { HeaderTwoComponent } from './common/headers/header-two/header-two.component';
-import { HeaderThreeComponent } from './common/headers/header-three/header-three.component';
+// import { HeaderTwoComponent } from './common/headers/header-two/header-two.component';
+// import { HeaderThreeComponent } from './common/headers/header-three/header-three.component';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from './common/layout/layout.component';
 import { FooterComponent } from './common/footer/footer/footer.component';
@@ -20,13 +20,13 @@ import { HeaderModalComponent } from './common/headers/header-modal/header-modal
   declarations: [
     FeatureModuleComponent,
     SideMenuOneComponent,
-    SideMenuTwoComponent,
+    // SideMenuTwoComponent,
     HeaderOneComponent,
     HeaderModalComponent,
     // SideMenuThreeComponent,
     SettingsMenuComponent,
-    HeaderTwoComponent,
-    HeaderThreeComponent,
+    // HeaderTwoComponent,
+    // HeaderThreeComponent,
     LayoutComponent,
     FooterComponent
   ],
