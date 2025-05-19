898cc191d065993b20d41e1ae35a680a0a8a5cfa
Rudra <nigamrudra761@gmail.com>
2025-05-20 00:49:20 +0530

    prod url change

--- src/app/services/login-component.service.ts
+++ src/app/services/login-component.service.ts
@@ -10,7 +10,7 @@ export class LoginComponentService {
 
 //  private baseUrl = 'http://103.168.18.28:8080'; //dev server
 //  private baseUrl = 'http://10.101.71.204:8080'; //Vedanta dev server
- private baseUrl = 'http://10.101.71.190:8080'; //vedanta Prod Server
+ private baseUrl = 'https://10.101.71.190:8080'; //vedanta Prod Server
 
   private secretKey = '1234567890123456'; // Must match Java key
   private iv = 'abcdefghijklmnop'; // Must match Java IV
