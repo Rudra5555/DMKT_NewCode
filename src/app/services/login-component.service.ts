a5d3da81f4d8181966957ca2cc753542f8f42255
Biplob-m <dasbiplob8116.com>
2025-06-17 23:34:33 +0530

    UserDelete

--- src/app/services/login-component.service.ts
+++ src/app/services/login-component.service.ts
@@ -8,10 +8,10 @@ import * as CryptoJS from 'crypto-js';
 })
 export class LoginComponentService {
 
-//  private baseUrl = 'http://103.168.18.28:8080'; //dev server
+ private baseUrl = 'http://103.168.18.28:8080'; //dev server
 //  private baseUrl = 'http://10.101.71.204:8080'; //Vedanta dev server
 // private baseUrl = 'https://10.101.71.190:8080'; //vedanta Prod Server
-private baseUrl = 'https://dmkt.balco.in:8080';
+// private baseUrl = 'https://dmkt.balco.in:8080';
 
   private secretKey = '1234567890123456'; // Must match Java key
   private iv = 'abcdefghijklmnop'; // Must match Java IV
@@ -59,6 +59,16 @@ deleteFile(fileName: any): Observable<any> {
     })
   );
 }
+
+
+// deleteUser(userId: any): Observable<any> {
+//   return this.http.delete(`${this.baseUrl}/`).pipe(
+//     catchError((error: any) => {
+//       console.error('Failed to delete file:', error);
+//       return throwError(error);
+//     })
+//   );
+// }
 
 
 
